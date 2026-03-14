917ced609243f193f9689a62602bd45fd5a523fb
Sachchidanand Kumar <sachchidanandkumar099@gmail.com>
2026-03-14 13:23:13 +0530

    first commit

---
+++ reactdemo/app.js
@@ -0,0 +1,10 @@
+// let h1 = document.createElement('h1');
+// let p = document.createElement('p');
+// h1.innerHTML = "hello sir";
+// p.innerHTML = "hello this is sachchidanand Kumar";
+// document.body.appendChild(h1);
+// document.body.appendChild(p);
+
+
+import array from './script.js';
+console.log(array);
